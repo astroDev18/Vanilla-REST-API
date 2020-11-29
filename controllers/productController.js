af60068d382e8794a58139fa0a34a3dd594d2ef4
astro_dev <isajaffarali@gmail.com>
2020-11-29 19:49:52 +0300

    Finished REST api

---
+++ controllers/productController.js
@@ -0,0 +1,116 @@
+const Product = require('../models/productModel')
+
+const { getPostData } = require('../utils')
+// @desc  Gets all products
+// @route Get /api/products
+async function getProducts( req, res ) {
+    try {
+        const products = await Product.findAll()
+
+        res.writeHead(200, { 'Content-Type': 'application/json' })
+        res.end(JSON.stringify(products))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+// @desc  Gets single products
+// @route Get /api/product/:id
+async function getProduct(req, res, id) {
+    try {
+        const product = await Product.findById(id)
+
+        if(!product) {
+            res.writeHead(404, { 'Content-Type': 'application/json' })
+            res.end(JSON.stringify({ message: 'Product Not Found' }))
+        } else {
+            res.writeHead(200, { 'Content-Type': 'application/json' })
+            res.end(JSON.stringify(product))
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+// @desc Create a product
+// @route POST /api/products
+
+async function createProduct( req, res) {
+    try {
+        const body = await getPostData(req);
+
+        const {title, description, price } = JSON.parse(body);
+
+        const product = {
+            title,
+            description,
+            price
+        }
+
+        const newProduct = await Product.create(product)
+
+        res.writeHead(201, {'Content-Type': 'application/json'})
+        return res.end(JSON.stringify(newProduct))
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+// @desc Create a product
+// @route POST /api/products
+
+async function updateProduct( req, res, id) {
+    try {
+        const product = await Product.findById(id);
+
+        if (!product) {
+            res.writeHead(404, { 'Content-Type': 'application/json' })
+            res.end(JSON.stringify({ message: 'Product Not Found' }))
+        } else {
+            const body = await getPostData(req);
+
+            const {title, description, price } = JSON.parse(body);
+
+            const productData = {
+                title: title || product.title,
+                description: description || product.description,
+                price: price || product.price
+            }
+
+            const updProduct = await Product.update(id, productData)
+
+            res.writeHead(200, {'Content-Type': 'application/json'})
+            return res.end(JSON.stringify(updProduct))
+
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+// @desc  Delete Product
+// @route delete request to /api/product/:id
+async function deleteProduct(req, res, id) {
+    try {
+        const product = await Product.findById(id)
+
+        if(!product) {
+            res.writeHead(404, { 'Content-Type': 'application/json' })
+            res.end(JSON.stringify({ message: 'Product Not Found' }))
+        } else {
+            await Product.remove(id);
+            res.writeHead(200, { 'Content-Type': 'application/json' })
+            res.end(JSON.stringify({ message: `Product ${id} removed`}))
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+module.exports = {
+    getProducts,
+    getProduct,
+    createProduct,
+    updateProduct,
+    deleteProduct
+}
